b1447f35ff0a661812a80e24189e0734a48a10b6
henimex <49072861+henimex@users.noreply.github.com>
2021-10-26 21:55:42 +0300

    Initial commit

---
+++ src/main/java/com/henimex/jspringrecap/JSpringRecapApplication.java
@@ -0,0 +1,13 @@
+package com.henimex.jspringrecap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JSpringRecapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JSpringRecapApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/henimex/jspringrecap/JSpringRecapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.henimex.jspringrecap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JSpringRecapApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
