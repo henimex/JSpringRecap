de84a095b531db16984e27288b8c6aa8c5b487d3
henimex <49072861+henimex@users.noreply.github.com>
2021-10-27 16:39:22 +0300

    At the end Swagger implemented

--- src/main/java/com/henimex/jspringrecap/JSpringRecapApplication.java
+++ src/main/java/com/henimex/jspringrecap/JSpringRecapApplication.java
@@ -2,6 +2,11 @@ package com.henimex.jspringrecap;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 public class JSpringRecapApplication {
@@ -10,5 +15,4 @@ public class JSpringRecapApplication {
         SpringApplication.run(JSpringRecapApplication.class, args);
         System.out.println("At The End a Spring Project Perfectly Runs With Connected PostgreDB");
     }
-
 }
---
+++ src/main/java/com/henimex/jspringrecap/configurations/SwaggerConfig.java
@@ -0,0 +1,39 @@
+package com.henimex.jspringrecap.configurations;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collections;
+
+@EnableSwagger2
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                //.apis(RequestHandlerSelectors.any())
+                .apis(RequestHandlerSelectors.basePackage("com.henimex.jspringrecap"))
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfo(
+                "Java SpringBoot Rest API",
+                "Using PostgreSQL Database by j.Henimex",
+                "a.1.0.21",
+                "Terms of Use Free",
+                new Contact("Joseph HenimeX", "call: 0543 502 43 26", "henimex@gmail.com"),
+                "Free License", "",
+                Collections.emptyList()
+        );
+    }
+}
